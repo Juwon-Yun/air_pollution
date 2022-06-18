b2c950b897d76788d32e7080b75c0a2c8968fc91
Juwon-Yun <eooll2@daum.net>
2022-06-19 01:14:04 +0900

    Test: unit test for dotenv property

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:air_pollution/screen/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 main() {
   runApp(MaterialApp(home: MainApp()));
+}
+
+initApp() {
+  print(dotenv.env['SERVICE_KEY']);
 }
---
+++ test/unit_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  test('test for dotenv property', () {
+    final serviceKey = dotenv.env['SERVICE_KEY'];
+
+    expect(serviceKey, 'null');
+  });
+}
