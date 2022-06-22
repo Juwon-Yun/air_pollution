eb9288c1a85d43399eb14935d367c0a4c5a93059
Juwon-Yun <eooll2@daum.net>
2022-06-22 16:26:46 +0900

    Feat: open  HiveField box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:air_pollution/constants/custom_theme.dart';
+import 'package:air_pollution/model/stat_model.dart';
 import 'package:air_pollution/screen/main_screen.dart';
-import 'package:air_pollution/screen/test_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,18 +9,25 @@ const testBox = 'test';
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
-
-  await Hive.openBox(testBox);
 
   await initController();
 
   runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TestScreen(),
-      theme: customTheme));
+      debugShowCheckedModeBanner: false, home: MainApp(), theme: customTheme));
 }
 
 initController() async {
   await dotenv.load(fileName: ".env");
+
+  await Hive.initFlutter();
+
+  // StatModel Type의 Hive 사용하기
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
+  await Hive.openBox(testBox);
+
+  for (ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
 }
